8e0726765e9fc0f3ecd6de40875df1e19f3bbb5e
Johannes Ziemke <github@5pi.de>
2022-11-10 12:39:35 +0100

    Support changing difficulty and characters

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""Diambra Arena agent"""
 import logging
 import argparse
 import time
@@ -15,36 +16,53 @@ logger = logging.getLogger(__name__)
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
 
+
 def main():
     """main entry point"""
     parser = argparse.ArgumentParser(
         prog='my agent',
         description='My agent competing in Diambra Arena.'
     )
+    parser.add_argument('--difficulty', type=int, help='difficulty level')
+    parser.add_argument('--characters', type=str, help='character name')
+
     subparsers = parser.add_subparsers(help='operations')
 
-    parser_train = subparsers.add_parser('train', help='Train a model to play Diambra Arena.')
+    parser_train = subparsers.add_parser(
+        'train', help='Train a model to play Diambra Arena.')
     parser_train.add_argument('--log-dir', type=str, default='logs')
-    parser_train.add_argument('--name-prefix', type=str, default=time.strftime("%Y%m%d-%H%M%S"))
+    parser_train.add_argument(
+        '--name-prefix', type=str, default=time.strftime("%Y%m%d-%H%M%S"))
     parser_train.add_argument('--load-agent-path', type=str)
     parser_train.set_defaults(func=train)
 
-    parser_play = subparsers.add_parser('play', help='Play a model to play Diambra Arena.')
+    parser_play = subparsers.add_parser(
+        'play', help='Play a model to play Diambra Arena.')
     parser_play.add_argument('agent_path', nargs=1, type=str)
     parser_play.set_defaults(func=play)
 
-
     args = parser.parse_args()
     args.func(args)
+
+
+def settings_from_args(args):
+    """create settings from argpase args"""
+    settings = {
+        "hardcore": True,
+        "frame_shape": [128, 128, 1],
+    }
+    if args.difficulty:
+        settings["difficulty"] = args.difficulty
+    if args.characters:
+        settings["characters"] = args.characters
+    return settings
+
 
 def train(args):
     """train subcommand"""
     env, num_envs = make_sb3_env(
         "doapp",
-        {
-            "hardcore": True,
-            "frame_shape": [128, 128, 1],
-        },
+        settings_from_args(args),
         {
             "reward_normalization": True,
             "frame_stack": 5,
@@ -60,19 +78,21 @@ def train(args):
 
     logger.info("Agent policy: %s", agent.policy)
 
-    agent.learn(total_timesteps=STEPS, callback=CheckpointCallback(N_PER_CHECKPOINT, args.log_dir, name_prefix=args.name_prefix))
+    agent.learn(
+        total_timesteps=STEPS,
+        callback=CheckpointCallback(
+            N_PER_CHECKPOINT, args.log_dir, name_prefix=args.name_prefix)
+    )
 
     env.close()
+
 
 def play(args):
     """play subcommand"""
     agent = PPO.load(args.agent_path[0])
     env, _ = make_sb3_env(
         "doapp",
-        {
-            "hardcore": True,
-            "frame_shape": [128, 128, 1],
-        },
+        settings_from_args(args),
         {
             "reward_normalization": True,
             "frame_stack": 5,
@@ -101,6 +121,7 @@ def play(args):
             break
 
     env.close()
+
 
 if __name__ == "__main__":
     main()
